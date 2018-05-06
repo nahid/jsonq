98f0ea98904a792b8e1996b4eaa04bf3e014c6dd
me-shaon <shaon.cse81@gmail.com>
2018-05-06 11:32:50 +0600

    Requiring path updated in Example file

--- example/index.php
+++ example/index.php
@@ -1,19 +1,19 @@
 <?php
-require '../vendor/autoload.php';
+
+$rootDir = str_replace('\\', '/', dirname(__DIR__, 1)) . DIRECTORY_SEPARATOR;
+
+require $rootDir .  'vendor/autoload.php';
 
 use Nahid\JsonQ\Jsonq;
 
 $json=new Jsonq();
-$json->import('../data.json');
-
+$json->import($rootDir . 'data.json');
 
 $result = $json->from('products')
-    ->where('cat', '=', 1)
-    ->fetch()
-    ->sortAs('price', 'desc')
-    ->first();
-            
+                ->where('cat', '=', 1)
+                ->fetch()
+                ->sortAs('price', 'desc')
+                ->first();
 
 echo '<pre>';
 dump($result);
-
